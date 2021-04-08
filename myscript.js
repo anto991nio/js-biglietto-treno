458e59a943c62b68a1a8e7f999bf03410d9e238a
anto991nio <antonio.91.cardone@gmail.com>
2021-04-08 16:53:57 +0200

    Termina esercizio

--- myscript.js
+++ myscript.js
@@ -6,25 +6,24 @@ console.log( etaPasseggero , kmPercorsi );
 var prezzoKm = 0.21;
 var prezzoBiglietto = kmPercorsi * prezzoKm;
 
-
 if(Number.isNaN (etaPasseggero) || Number.isNaN(kmPercorsi)){
     alert("Dati immessi non corretti")
 }else{
 
     if (etaPasseggero < 18 ){
     console.log( prezzoBiglietto - prezzoBiglietto * 0.20 )
-    document.getElementById("Biglietto").innerHTML = "Il costo del biglietto è "+ "" + (prezzoBiglietto - prezzoBiglietto * 0.20 ) ;
+    document.getElementById("Biglietto").innerHTML = "Il costo del biglietto è "+ "" + (prezzoBiglietto - prezzoBiglietto * 0.20 ).toFixed(2) ;
     } 
 
 
     else if (etaPasseggero > 65) {
     console.log( prezzoBiglietto - prezzoBiglietto * 0.40 )
-    document.getElementById("Biglietto").innerHTML = "Il costo del biglietto è "+ "" + (prezzoBiglietto - prezzoBiglietto * 0.40 ) ;
+    document.getElementById("Biglietto").innerHTML = "Il costo del biglietto è "+ "" + (prezzoBiglietto - prezzoBiglietto * 0.40 ).toFixed(2) ;
     }
 
     else{
     console.log(prezzoBiglietto);
-    document.getElementById("Biglietto").innerHTML = "Il costo del biglietto è "+ "" + prezzoBiglietto ;
+    document.getElementById("Biglietto").innerHTML = "Il costo del biglietto è "+ "" + prezzoBiglietto.toFixed(2) ;
     }
 }
 
